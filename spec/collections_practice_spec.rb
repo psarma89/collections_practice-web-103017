700ae07904f39ec5b23d454c7cdb66db9fb7e54a
psarma89 <priyam.sarma@gmail.com>
2017-10-30 22:48:33 -0400

    Add solution with advanced questions and thier answers

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4 Advanced
+  describe '#swap_elements_from_to' do
+    it 'swap the index and destination_index elements of an array' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"],1,2)).to eq(["blake", "scott", "ashley"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
